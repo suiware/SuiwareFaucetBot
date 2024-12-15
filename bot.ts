4a2521d7fd7a5a75fd56d3494eded73d800e3777
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-12-15 19:21:40 +0100

    Updated the welcome message.

--- bot.ts
+++ bot.ts
@@ -5,7 +5,7 @@ const bot = new Bot(Deno.env.get("BOT_KEY") || "");
 
 bot.command("start", (ctx) =>
   ctx.reply(
-    "Welcome to the Sui Faucet by Suiware!\n\n Type `devnet [address]` or `testnet [address]` to get some test SUI."
+    "Welcome to the Sui Faucet by Suiware!\n---\n\nUse **devnet [address]** or **testnet [address]** to fund your address."
   )
 );
 
