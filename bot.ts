c0566f9f7f0e9b103add5745901a4e6181b969d1
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-12-15 16:20:42 +0100

    Formatted the code.

--- bot.ts
+++ bot.ts
@@ -1,7 +1,7 @@
 import { Bot } from "https://deno.land/x/grammy@v1.33.0/mod.ts";
 import "jsr:@std/dotenv/load";
 
-const bot = new Bot(Deno.env.get("BOT_KEY") || '');
+const bot = new Bot(Deno.env.get("BOT_KEY") || "");
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
