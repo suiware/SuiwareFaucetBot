d94d9195ec2d60ca95f50da7937d16b4c76e29c7
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-12-15 17:26:03 +0100

    Working on the faucet

--- bot.ts
+++ bot.ts
@@ -3,7 +3,24 @@ import "jsr:@std/dotenv/load";
 
 const bot = new Bot(Deno.env.get("BOT_KEY") || "");
 
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+bot.command("start", (ctx) =>
+  ctx.reply(
+    "Welcome to the Sui Faucet by Suiware!\n\n Type `devnet [address]` or `testnet [address]` to get some test SUI."
+  )
+);
+
+bot.on("message", (ctx) => {
+  if (ctx.message?.text?.startsWith("devnet")) {
+    ctx.reply("Dev mode is not available yet.");
+    return;
+  }
+
+  if (ctx.message?.text?.startsWith("testnet")) {
+    ctx.reply("Test mode is not available yet.");
+    return;
+  }
+
+  return ctx.reply("Echo: " + ctx.message?.text);
+});
 
 bot.start();
