e1260cf679c31b2c67704c53cdec1b55924f3fd1
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-12-16 11:01:28 +0100

    Removed devnet and testnet commands from the menu.

--- constants.ts
+++ constants.ts
@@ -1,13 +1,13 @@
 export const MENU = [
   { command: "start", description: "Intro" },
   { command: "help", description: "Help info" },
-  { command: "devnet 0x...", description: "Devnet faucet. Usage: /devnet 0x..." },
-  { command: "testnet 0x...", description: "Testnet faucet. Usage: /testnet 0x..." },
+  // { command: "devnet", description: "Devnet faucet. Usage: /devnet 0x..." },
+  // { command: "testnet", description: "Testnet faucet. Usage: /testnet 0x..." },
 ];
 
 export const WELCOME_MESSAGE = "Welcome to Sui Faucet by Suiware.io!";
 
 export const HELP_MESSAGE =
-  'Type "/devnet 0x..." or "/testnet 0x..." to get some test SUI.';
+  'Type "\/devnet 0x..." or "\/testnet 0x..." to get some test SUI.';
 
 export const INVALID_COMMAND_MESSAGE = `Invalid command. ${HELP_MESSAGE}`;
